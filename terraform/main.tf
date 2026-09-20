b0dacc3a6444b487a9c95284e848aebfe5925e72
paul-create <ruban.paul@gmail.com>
2026-09-20 14:27:01 +0100

    Terraform: Update main.tf with module references

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,11 +16,16 @@ module "iam" {
 }
 
 module "rds" {
-  source = "./modules/rds"
+  source      = "./modules/rds"
+  db_name     = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
 }
 
 module "s3" {
-  source = "./modules/s3"
+  source            = "./modules/s3"
+  s3_bucket_name    = var.s3_bucket_name
+  aws_region        = var.aws_region
 }
 
 module "cognito" {
